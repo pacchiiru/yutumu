e2858ace038e3f2b743912dcd282f45b49787449
pacchiiru <183164231+pacchiiru@users.noreply.github.com>
2025-03-06 00:51:14 -0500

    Adjust yup constants
    Add margin-top to yu-cl-main-view-blurb

--- src/const/yup-constants.ts
+++ src/const/yup-constants.ts
@@ -1,7 +1,7 @@
 export const YupConstants = {
     MIN_WIDTH: 300,
-    MAX_WIDTH: 1000,
-    MIN_HEIGHT: 50,
+    MAX_WIDTH: 1200,
+    MIN_HEIGHT: 45,
     MAX_HEIGHT: 1000,
     POS_PADDING: 10,
 } as const;
